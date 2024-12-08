1a01255631fc59bd637cdeb477002101d16c3036
AdAndRoll <vanovano03@mail.ru>
2024-12-08 23:03:29 +0300

    попытка с API, не увенчалась успехом, но добавлены зависимости, попробывать завтра

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,16 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.converter.gson)
+    implementation(libs.retrofit)
+    implementation(libs.picasso)
+    implementation(libs.okhttp)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
